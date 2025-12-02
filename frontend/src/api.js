2f3ccce530eb0b621dfd74b6607f84d0312b9561
Seema Anjum <163408846+Seema-Anjum@users.noreply.github.com>
2025-12-02 07:39:26 +0530

    Change API base URL to production server

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",   // backend
+  baseURL: "https://e-commerce-backend-pxls.onrender.com",   // backend
   withCredentials: true               // for cookies (JWT)
 });
 
